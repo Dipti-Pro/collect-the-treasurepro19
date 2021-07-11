44b741c7d2a23e93c9d27dfda9416cfd3a1c47d9
SHUBHAM SUDAM MHASKE <85775945+Shubham392007@users.noreply.github.com>
2021-07-11 23:07:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -135,7 +135,7 @@ function createJwellery() {
 }
 
 function createSword(){
-  if (World.frameCount % 30 == 0) {
+  if (World.frameCount % 530 == 0) {
   var sword = createSprite(Math.round(random(50, width-50),40, 10, 10));
   sword.addImage(swordImg);
   sword.scale=0.1;
